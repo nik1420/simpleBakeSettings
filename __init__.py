a189728d1e959ebb1587c1686c79c4140d9f5fb1
NikitaZotov <zonker413@gmail.com>
2025-01-08 11:05:41 +0500

    добавлена проверка на присутсвие материало, проход по всем материалам на объекте

--- __init__.py
+++ __init__.py
@@ -5,48 +5,51 @@ import mathutils
 import time
 res = 1024
 # Оператор для выполнения действия
+
 class RenderSettBC(bpy.types.Operator):
     bl_idname = "object.rendersettbc"
     bl_label = "Set Simple bake settings"
     
     def execute(self,context):
-        bpy.context.scene.render.engine = 'CYCLES'
-        #выставление настроек рендера
-        cyc_sett = bpy.data.scenes["Scene"].cycles
-        cyc_sett.device = "GPU"
-        cyc_sett.use_adaptive_sampling = False
-        cyc_sett.use_denoising = False
-        cyc_sett.samples = 10
-        cyc_sett.bake_type = 'DIFFUSE'
-        bpy.data.scenes["Scene"].render.bake.use_pass_direct = False
-        bpy.data.scenes["Scene"].render.bake.use_pass_indirect = False
-        #настройка материала
         cur_obj = bpy.context.active_object#находим выбранный объект
-        node_tree = cur_obj.active_material.node_tree#лезем в новы
-        nodes = node_tree.nodes#и в дерево
-        target_label = 'lbl'
-        res = bpy.data.scenes["Scene"].render.resolution_x#разрешение для запекания
+        if(len(cur_obj.data.materials)>0):#если есть материал
+            for index, material in enumerate(cur_obj.data.materials):
+                bpy.context.scene.render.engine = 'CYCLES'
+                #выставление настроек рендера
+                cyc_sett = bpy.data.scenes["Scene"].cycles
+                cyc_sett.device = "GPU"
+                cyc_sett.use_adaptive_sampling = False
+                cyc_sett.use_denoising = False
+                cyc_sett.samples = 10
+                cyc_sett.bake_type = 'DIFFUSE'
+                bpy.data.scenes["Scene"].render.bake.use_pass_direct = False
+                bpy.data.scenes["Scene"].render.bake.use_pass_indirect = False
+                #настройка материала
+                node_tree = material.node_tree#лезем в ноды
+                nodes = node_tree.nodes#и в дерево
+                target_label = 'lbl'
+                res = bpy.data.scenes["Scene"].render.resolution_x#разрешение для запекания
 
-        if node_tree:
-            # Ищем узел с указанным лейблом чтоб не создовать несколько
-            found_node = None
-            for node in node_tree.nodes:
-                if node.label == target_label:
-                    found_node = node
-                    node_tree.nodes.active = found_node
-                    break
-        if found_node:
-            pass
-        else:
-            texture_image_my = nodes.new(type="ShaderNodeTexImage")#создаем  ноду картинки
-            texture_image_my.label = "lbl"
-            bake_img = bpy.ops.image.new(name = "Bake",width=res,height=res)#создаем картинку
-            node_tree.nodes.active = texture_image_my
-            texture_image_my.select = True#делаем выбранной
+                if node_tree:
+                    # Ищем узел с указанным лейблом чтоб не создовать несколько
+                    found_node = None
+                    for node in node_tree.nodes:
+                        if node.label == target_label:
+                            found_node = node
+                            node_tree.nodes.active = found_node
+                            break
+                if found_node:
+                    pass
+                else:
+                    texture_image_my = nodes.new(type="ShaderNodeTexImage")#создаем  ноду картинки
+                    texture_image_my.label = "lbl"
+                    bake_img = bpy.ops.image.new(name = "Bake",width=res,height=res)#создаем картинку
+                    node_tree.nodes.active = texture_image_my
+                    texture_image_my.select = True#делаем выбранной
+                        
+                node_tree.nodes.active.image = bpy.data.images["Bake"]#ставим в выбранную картинку
                 
-        node_tree.nodes.active.image = bpy.data.images["Bake"]#ставим в выбранную картинку
-        
-        bpy.ops.object.bake(type="DIFFUSE",use_clear= True)
+                bpy.ops.object.bake(type="DIFFUSE",use_clear= True)
         return {'FINISHED'}
 
 
@@ -55,34 +58,38 @@ class RenderSettEmi(bpy.types.Operator):
     bl_label = "Set Simple bake settings emi"
     
     def execute(self,context):
-        cyc_sett = bpy.data.scenes["Scene"].cycles
-        cyc_sett.bake_type = 'EMIT'
         cur_obj = bpy.context.active_object#находим выбранный объект
-        node_tree = cur_obj.active_material.node_tree#лезем в новы
-        nodes = node_tree.nodes#и в дерево
-        target_label = 'lbl'
-        res = bpy.data.scenes["Scene"].render.resolution_x#разрешение для запекания
-
-        if node_tree:
-            # Ищем узел с указанным лейблом чтоб не создовать несколько
-            found_node = None
-            for node in node_tree.nodes:
-                if node.label == target_label:
-                    found_node = node
-                    node_tree.nodes.active = found_node
-                    break
-        if found_node:
-            pass
-        else:
-            texture_image_my = nodes.new(type="ShaderNodeTexImage")#создаем  ноду картинки
-            texture_image_my.label = "lbl"
-            bake_img = bpy.ops.image.new(name = "Bake",width=res,height=res)#создаем картинку
-            node_tree.nodes.active = texture_image_my
-            texture_image_my.select = True#делаем выбранной
+        if(len(cur_obj.data.materials)>0):#если есть материал
+            for index, material in enumerate(cur_obj.data.materials):
+                cyc_sett = bpy.data.scenes["Scene"].cycles
+                cyc_sett.bake_type = 'EMIT'
+                cur_obj = bpy.context.active_object#находим выбранный объект
+                node_tree = material.node_tree#лезем в ноды
+                nodes = node_tree.nodes#и в дерево
+                target_label = 'lbl'
+                res = bpy.data.scenes["Scene"].render.resolution_x#разрешение для запекания
+                if node_tree:
+                    # Ищем узел с указанным лейблом чтоб не создовать несколько
+                    found_node = None
+                    for node in node_tree.nodes:
+                        if node.label == target_label:
+                            found_node = node
+                            node_tree.nodes.active = found_node
+                            break
+                if found_node:
+                    pass
+                else:
+                    texture_image_my = nodes.new(type="ShaderNodeTexImage")#создаем  ноду картинки
+                    texture_image_my.label = "lbl"
+                    bake_img = bpy.ops.image.new(name = "Bake",width=res,height=res)#создаем картинку
+                    node_tree.nodes.active = texture_image_my
+                    texture_image_my.select = True#делаем выбранной
+                        
+                node_tree.nodes.active.image = bpy.data.images["Bake"]#ставим в выбранную картинку
                 
-        node_tree.nodes.active.image = bpy.data.images["Bake"]#ставим в выбранную картинку
-        
-        bpy.ops.object.bake(type="EMIT",use_clear= True)
+                bpy.ops.object.bake(type="EMIT",use_clear= True)
+        else:
+            pass
         return {'FINISHED'}
 
 # Панель для добавления кнопки
