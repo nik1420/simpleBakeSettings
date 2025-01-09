f97f57d6416c9afb8587b38b657caa4352c2459a
NikitaZotov <zonker413@gmail.com>
2025-01-09 07:06:01 +0500

    добавлен рендер нормала

--- __init__.py
+++ __init__.py
@@ -23,6 +23,7 @@ class RenderBC(bpy.types.Operator):#Метод для РЕНДЕРА цвета 
         old_cam = context.scene.camera
         context.scene.camera = camera_obj
         context.scene.view_layers["ViewLayer"].use_pass_diffuse_color = True#включаем пасс цвета в слоях
+        context.scene.view_layers["ViewLayer"].use_pass_normal = True# вкючаем пас нормала
         context.scene.use_nodes = True
         node_tree = context.scene.node_tree
         context.scene.render.engine = 'BLENDER_EEVEE_NEXT'#включаем еву
@@ -49,7 +50,7 @@ class RenderBC(bpy.types.Operator):#Метод для РЕНДЕРА цвета 
         context.scene.camera = old_cam
         return {'FINISHED'}
 
-class RenderSettBC(bpy.types.Operator):
+class RenderSettBC(bpy.types.Operator):##Запекание цвета
     bl_idname = "object.rendersettbc"
     bl_label = "Simple Bake BC"
     
@@ -127,7 +128,7 @@ class RenderSettBC(bpy.types.Operator):
         return {'FINISHED'}
 
 
-class RenderSettEmi(bpy.types.Operator):
+class RenderSettEmi(bpy.types.Operator):##Запекание емисии
     bl_idname = "object.rendersettemi"
     bl_label = "Simple Bake Emi"
     
@@ -201,6 +202,81 @@ class RenderSettEmi(bpy.types.Operator):
                             found_node1 = node
                             node_tree.nodes.remove(found_node1)
         return {'FINISHED'}
+    
+class RenderSettNorm(bpy.types.Operator):##Запекание нормала
+    bl_idname = "object.rendersettnorm"
+    bl_label = "Simple Bake Normal"
+    
+    def execute(self,context):
+        bake_target_label = context.active_object.simple_bake_image_name
+        bake_target_label_uv = bake_target_label + "_uv"
+        cur_obj = bpy.context.active_object#находим выбранный объект
+        cyc_sett = bpy.data.scenes["Scene"].cycles
+        cyc_sett.bake_type = 'NORMAL'
+        cyc_sett = context.scene.cycles
+        cyc_sett.device = "GPU"
+        cyc_sett.use_adaptive_sampling = False
+        cyc_sett.use_denoising = False
+        cyc_sett.samples = 10
+        context.scene.render.engine = 'CYCLES'
+        bake_resolution = int(context.active_object.simple_bake_resolution)
+        found_image = False
+        for image in bpy.data.images:
+            if(image.name == bake_target_label):
+                found_image = True
+                break
+        if(found_image == False):
+            bake_img = bpy.ops.image.new(name = bake_target_label,width=bake_resolution,height=bake_resolution)#создаем картинку
+        if(len(cur_obj.data.materials)>0):#если есть материал
+            for index, material in enumerate(cur_obj.data.materials):
+                node_tree = material.node_tree#лезем в ноды
+                nodes = node_tree.nodes#и в дерево
+                if node_tree:
+                    # Ищем узел с указанным лейблом чтоб не создовать несколько
+                    found_node = None
+                    for node in node_tree.nodes:
+                        if node.label == bake_target_label:
+                            found_node = node
+                            node_tree.nodes.active = found_node
+                        if node.label == bake_target_label_uv:
+                            found_node1 = node
+                            found_node1.uv_map = cur_obj.data.uv_layers.active.name
+                            break
+                        else:
+                            texture_image_my = nodes.new(type="ShaderNodeTexImage")#создаем  ноду картинки
+                            texture_image_my.label = bake_target_label
+
+                            uv_map_node  = nodes.new(type="ShaderNodeUVMap")#создаем ноду юв
+                            uv_map_node.label = bake_target_label_uv
+                            uv_map_node.uv_map = cur_obj.data.uv_layers.active.name#выбираем юв
+
+                            node_tree.links.new(uv_map_node.outputs['UV'],texture_image_my.inputs['Vector'])#соединяем юв и картинку
+                            bake_resolution = int(context.active_object.simple_bake_resolution)
+                            
+                            node_tree.nodes.active = texture_image_my#делаем активной
+                            node_tree.nodes.active.image = bpy.data.images[bake_target_label]#ставим в выбранную картинку    
+                            break
+        bpy.ops.object.bake(type="NORMAL",use_clear= True) 
+        ########удаление использованного из материала
+        if(len(cur_obj.data.materials)>0):#если есть материал
+            for index, material in enumerate(cur_obj.data.materials):
+                #настройка материала
+                node_tree = material.node_tree#лезем в ноды
+                nodes = node_tree.nodes#и в дерево
+                if node_tree:
+                    # Ищем узел с указанным лейблом чтоб не создовать несколько
+                    found_node = None
+                    found_node1 = None
+                    for node in node_tree.nodes:
+                        if node.label == bake_target_label:
+                            found_node = node
+                            node_tree.nodes.remove(found_node)
+                        
+                    for node in node_tree.nodes:
+                        if node.label == bake_target_label_uv:
+                            found_node1 = node
+                            node_tree.nodes.remove(found_node1)
+        return {'FINISHED'}
 
 # Панель для добавления кнопки
 class OBJECT_PT_CustomPanel(bpy.types.Panel):
@@ -222,6 +298,7 @@ class OBJECT_PT_CustomPanel(bpy.types.Panel):
                 layout.prop(bpy.context.active_object.data.uv_layers,'active_index',text = "UV Map")
                 layout.operator("object.rendersettbc", icon='RESTRICT_RENDER_OFF')
                 layout.operator("object.rendersettemi", icon='RESTRICT_RENDER_OFF')
+                layout.operator("object.rendersettnorm", icon='RESTRICT_RENDER_OFF')
                 layout.split(factor=0.1)
                 box = layout.box()
                 row = box.row()
@@ -235,6 +312,7 @@ class OBJECT_PT_CustomPanel(bpy.types.Panel):
 def register():
     bpy.utils.register_class(RenderSettEmi)
     bpy.utils.register_class(RenderSettBC)
+    bpy.utils.register_class(RenderSettNorm)
     bpy.utils.register_class(OBJECT_PT_CustomPanel)
     bpy.utils.register_class(RenderBC)
     bpy.types.Object.simple_bake_resolution = EnumProperty(
@@ -263,6 +341,7 @@ def register():
 def unregister():
     bpy.utils.unregister_class(RenderSettEmi)
     bpy.utils.unregister_class(RenderSettBC)
+    bpy.utils.unregister_class(RenderSettNorm)
     bpy.utils.unregister_class(OBJECT_PT_CustomPanel)
     bpy.utils.unregister_class(RenderBC)
     del bpy.types.Object.simple_bake_resolution
