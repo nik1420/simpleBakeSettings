05102e31840adb1875c9c1afea8c9bcacf50ab45
NikitaZotov <zonker413@gmail.com>
2025-10-24 18:25:49 +0500

    правки

--- __init__.py
+++ __init__.py
@@ -844,10 +844,9 @@ class RenderSettRMA(bpy.types.Operator):##Запекание емисии
                             found_node1 = node
                             node_tree.nodes.remove(found_node1)
 
-
         samples = int(context.active_object.samples)
-        bake_target_label = context.active_object.simple_bake_image_name
-        bake_target_label_uv = bake_target_label + "_uv"
+        bake_target_label_m = context.active_object.simple_bake_image_name + '_M'
+        bake_target_label_uv = bake_target_label_m + "_uv"
         cur_obj = context.active_object#находим выбранный объект
         #выставление настроек рендера
         cyc_sett = context.scene.cycles
@@ -866,14 +865,14 @@ class RenderSettRMA(bpy.types.Operator):##Запекание емисии
         found_image = False
         mats_bc = [None] * len(cur_obj.data.materials)
         for image in bpy.data.images:
-            if(image.name == bake_target_label):#если картинка уже существовала
-                img = bpy.data.images.get(bake_target_label)
+            if(image.name == bake_target_label_m):#если картинка уже существовала
+                img = bpy.data.images.get(bake_target_label_m)
                 bpy.data.images.remove(img)#удаляем ее
                 found_image = False
                 break
         if(found_image == False):
-            bake_img = bpy.ops.image.new(name = bake_target_label,width=bake_resolution,height=bake_resolution)#создаем картинку
-            bpy.data.images[bake_target_label].colorspace_settings.name = "Non-Color"#назначаем нужный цветовой профиль
+            bake_img = bpy.ops.image.new(name = bake_target_label_m,width=bake_resolution,height=bake_resolution)#создаем картинку
+            bpy.data.images[bake_target_label_m].colorspace_settings.name = "Non-Color"#назначаем нужный цветовой профиль
         if(len(cur_obj.data.materials)>0):#если есть материал
             for index, material in enumerate(cur_obj.data.materials):
                 #настройка материала
@@ -884,7 +883,7 @@ class RenderSettRMA(bpy.types.Operator):##Запекание емисии
                     found_node = None
                     found_node1 = None
                     for node in node_tree.nodes:
-                        if node.label == bake_target_label:
+                        if node.label == bake_target_label_m:
                             found_node = node
                             node_tree.nodes.active = found_node
                         if node.label == bake_target_label_uv:
@@ -915,15 +914,16 @@ class RenderSettRMA(bpy.types.Operator):##Запекание емисии
                                 node_tree.links.new(connected_node_metalic.outputs[connected_socket_metalic],principled_node.inputs[27])#соединяем с emission color
 
                             texture_image_my = nodes.new(type="ShaderNodeTexImage")#создаем  ноду картинки
-                            texture_image_my.label = bake_target_label
+                            texture_image_my.label = bake_target_label_m
                             uv_map_node  = nodes.new(type="ShaderNodeUVMap")#создаем ноду юв
                             uv_map_node.label = bake_target_label_uv
                             uv_map_node.uv_map = cur_obj.data.uv_layers.active.name#выбираем юв
                             node_tree.links.new(uv_map_node.outputs['UV'],texture_image_my.inputs['Vector'])#соединяем юв и картинку
                             node_tree.nodes.active = texture_image_my#делаем активной
-                            node_tree.nodes.active.image = bpy.data.images[bake_target_label]#ставим в выбранную картинку
+                            node_tree.nodes.active.image = bpy.data.images[bake_target_label_m]#ставим в выбранную картинку
                             break
         bpy.ops.object.bake(type="EMIT",use_clear= True) 
+        bpy.types.Scene.m_name = bake_target_label_m
         ############################################################################################Вертаем взад
         if(len(cur_obj.data.materials)>0):#если есть материал
             for index, material in enumerate(cur_obj.data.materials):
