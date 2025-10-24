815b8b1b092afe05ebf1a34e0b584b20da5a852c
NikitaZotov <zonker413@gmail.com>
2025-10-24 23:33:31 +0500

    и ao тоже

--- __init__.py
+++ __init__.py
@@ -300,7 +300,7 @@ class RenderSettAO(bpy.types.Operator):##Запекание цвета
                     found_image = False
                     break
         if(found_image == False):
-            bake_img = bpy.ops.image.new(name = bake_target_label,width=bake_resolution,height=bake_resolution)#создаем картинку
+            bake_img = bpy.ops.image.new(name = bake_target_label,width=bake_resolution,height=bake_resolution,float = True)#создаем картинку
             bpy.data.images[bake_target_label].colorspace_settings.name = "Non-Color"#назначаем нужный цветовой профиль
         if(len(cur_obj.data.materials)>0):#если есть материал
             for index, material in enumerate(cur_obj.data.materials):
