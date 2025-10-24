47272bd8c259b688eed1390737507fbffc855e19
NikitaZotov <zonker413@gmail.com>
2025-10-24 18:51:05 +0500

    правки

--- __init__.py
+++ __init__.py
@@ -790,7 +790,7 @@ class RenderSettRMA(bpy.types.Operator):##Запекание емисии
                     found_image = False
                     break
         if(found_image == False):
-            bake_img_r = bpy.ops.image.new(name = bake_target_label_R,width=bake_resolution,height=bake_resolution,float = '32')#создаем картинку
+            bake_img_r = bpy.ops.image.new(name = bake_target_label_R,width=bake_resolution,height=bake_resolution,float = True)#создаем картинку
             bpy.data.images[bake_target_label_R].colorspace_settings.name = "Non-Color"#назначаем нужный цветовой профиль
         if(len(cur_obj.data.materials)>0):#если есть материал
             for index, material in enumerate(cur_obj.data.materials):
